77322e62241ec7c7957a44b6bbdf1b8767922d66
ugalracing <g12460778@tdrive.thu.edu.tw>
2024-03-11 12:05:43 +0800

    add calculator.py

---
+++ calculator.py
@@ -0,0 +1,11 @@
+# Theodore
+# Calculator to add two numbers and display the result
+# 1. Input
+
+x1 = input("type x1:")
+x2 = input("type x2:")
+
+# 2. Process
+sum=int(x1) + int(x2)
+# 3. Output
+print(f"the sum of {x1} and {x2} = ", sum)
