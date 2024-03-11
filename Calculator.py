62662a57cf256ac38df0e41a821e74131f4a5a11
THU GMBA <researchclock@gmail.com>
2024-03-11 11:53:17 +0800

    Add Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+# Brian 
+# Calculator to add two numbers and display the result 
+
+# 1. Input
+
+x1 = input("Type x1: ")
+x2 = input("Type x2: ")
+
+# 2. Process
+
+sum = int(x1) + int(x2)
+
+# 3. OUtput
+
+print(f"Sum: {sum}")
